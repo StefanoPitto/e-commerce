70d0c8ff4de91f4b7470c01a7558d7078c5bfeb8
StefanoPitto <StefanoPitto@hotmail.com.ar>
2021-07-16 18:20:45 -0300

    Visual updates

--- src/Components/ItemDetail.js
+++ src/Components/ItemDetail.js
@@ -1,13 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import { Container, Button } from "@material-ui/core";
+import ItemCount from "./ItemCount"
 
-const StyledDiv = styled.div``;
+const StyledContainer = styled(Container)`
+  max-width: 900px;
+  border: 1px solid red;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction:row;
 
+`;
+
+const ImageDiv= styled.div`
+img{
+  width: 100%;
+}
+`;
+
+const ButtonsDiv= styled.div`
+  min-width: 200px;
+`;
 const ItemDetail = (props) => {
   return (
-    <StyledDiv>
-      <h1>{props.info.productId}</h1>
-    </StyledDiv>
+    <StyledContainer>
+      <ImageDiv>
+       <img src={`${props.info.productImage}`} />
+      </ImageDiv>      
+      <ButtonsDiv>
+      <ItemCount/>
+      <Button variant="contained">Agregar al carrito</Button>
+      </ButtonsDiv>    
+    </StyledContainer>
   );
 };
 
