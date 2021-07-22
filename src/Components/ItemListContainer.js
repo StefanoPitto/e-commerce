b0965c32ca4de0043430786ed50c7a796e1545e3
StefanoPitto <stefapitto@hotmail.com.ar>
2021-07-22 19:39:00 -0300

    New products

--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -6,52 +6,58 @@ import ProductNavBar from "./ProductsNavBar";
 import { useParams } from "react-router-dom";
 
 const ItemContainer = styled.div`
-	max-width: 900px;
-	margin: 0 auto;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Loading = styled(LinearProgress)`
-	width: 100vw;
-	height: 4px;
-	background-color: #000000;
+  width: 100vw;
+  height: 4px;
+  background-color: #000000;
 `;
 
 const StyledDiv = styled.div``;
 
 const ItemListContainer = (props) => {
-	const { categoryID } = useParams();
-	const [products, setProducts] = useState();
-	const getProducts = async () => {
-		let productos = await fetch("/json/products.json");
-		let responseProducts = await productos.json();
-		if (categoryID !== "todo") {
-			let newArray = responseProducts.filter(
-				(product) => product.productType === categoryID
-			);
-			setProducts(newArray);
-		} else {
-			setProducts(responseProducts);
-		}
-	};
-	useEffect(() => {
-		getProducts();
-	});
+  const { categoryID } = useParams();
+  const [products, setProducts] = useState();
+  useEffect(() => {
+    let componentMounted = true;
+    const getProducts = async () => {
+      let productos = await fetch("/json/products.json");
+      let responseProducts = await productos.json();
+      if (categoryID !== "todo") {
+        let newArray = responseProducts.filter(
+          (product) => product.productType === categoryID
+        );
+        setProducts(newArray);
+      } else {
+        setProducts(responseProducts);
+      }
+    };
+    if (componentMounted) {
+      getProducts();
+    }
+    return () => {
+      componentMounted = false;
+    };
+  });
 
-	return (
-		<ItemContainer>
-			{products === undefined ? (
-				<Loading color="secondary"></Loading>
-			) : (
-				<StyledDiv>
-					<ProductNavBar />
-					<ItemList products={products}> </ItemList>
-				</StyledDiv>
-			)}
-		</ItemContainer>
-	);
+  return (
+    <ItemContainer>
+      {products === undefined ? (
+        <Loading color="secondary"></Loading>
+      ) : (
+        <StyledDiv>
+          <ProductNavBar />
+          <ItemList products={products}> </ItemList>
+        </StyledDiv>
+      )}
+    </ItemContainer>
+  );
 };
 
 export default ItemListContainer;
--- src/Components/ProductsNavBar.js
+++ src/Components/ProductsNavBar.js
@@ -5,46 +5,46 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const StyledTab = styled(Tabs)`
-	&:focus {
-		background-color: red;
-	}
-	span {
-		text-align: center;
-	}
-	a {
-		text-decoration: none;
-		color: inherit;
-	}
+  &:focus {
+    background-color: red;
+  }
+  span {
+    text-align: center;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 `;
 
 const ProductNavBar = (props) => {
-	const [value, setValue] = useState(0);
-	const handleClick = (elem) => {
-		setValue(elem);
-	};
+  const [value, setValue] = useState(0);
+  const handleClick = (elem) => {
+    setValue(elem);
+  };
 
-	return (
-		<StyledTab value={value} variant="fullWidth" indicatorColor="primary">
-			<Link to="/products/todo">
-				<Tab label="Todo" onClick={() => handleClick(0, "todo")} />
-			</Link>
-			<Link to="/products/remeras">
-				<Tab onClick={() => handleClick(1)} label="Remeras" />
-			</Link>
-			<Link to="/products/pantalones">
-				<Tab onClick={() => handleClick(2)} label="Pantalones" />
-			</Link>
-			<Link to="/products/trajes">
-				<Tab onClick={() => handleClick(3)} label="Trajes" />
-			</Link>
-			<Link to="/products/calzado">
-				<Tab onClick={() => handleClick(4)} label="Calzado" />
-			</Link>
-			<Link to="/products/buzos">
-				<Tab onClick={() => handleClick(5)} label="Buzos" />
-			</Link>
-		</StyledTab>
-	);
+  return (
+    <StyledTab value={value} indicatorColor="primary">
+      <Link to="/products/todo">
+        <Tab label="Todo" onClick={() => handleClick(0, "todo")} />
+      </Link>
+      <Link to="/products/remeras">
+        <Tab onClick={() => handleClick(1)} label="Remeras" />
+      </Link>
+      <Link to="/products/pantalones">
+        <Tab onClick={() => handleClick(2)} label="Pantalones" />
+      </Link>
+      <Link to="/products/camperas">
+        <Tab onClick={() => handleClick(3)} label="Camperas" />
+      </Link>
+      <Link to="/products/calzado">
+        <Tab onClick={() => handleClick(4)} label="Calzado" />
+      </Link>
+      <Link to="/products/buzos">
+        <Tab onClick={() => handleClick(5)} label="Buzos" />
+      </Link>
+    </StyledTab>
+  );
 };
 
 export default ProductNavBar;
